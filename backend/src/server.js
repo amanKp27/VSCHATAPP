ae1cbe72bfca74773418473eb45b061c863c6114
Aman Kumar Poddar <amankrpoddar18@gmail.com>
2026-02-18 18:57:01 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
+
 
 app.use('/api/auth',authRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get(/^(.*)$/,(req,res) => {
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
+    })
+}
 
 app.listen(process.env.PORT,()=> {
     console.log(`Port running on ${process.env.PORT}!`);
