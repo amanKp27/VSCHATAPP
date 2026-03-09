fb2d581ceab64ff7854f5d3bf77caa4148cd6e2f
Aman Kumar Poddar <amankrpoddar18@gmail.com>
2026-03-09 19:20:41 +0530

    update profile endpoint added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,6 +3,7 @@ import bcrypt from "bcrypt";
 import {generateToken} from "../lib/utils.js";
 import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 import {ENV} from "../lib/env.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async(req,res) => {
     const {fullName,email,password} = req.body;
@@ -78,6 +79,12 @@ export const signup = async(req,res) => {
 export const login = async (req,res) => {
     const {email,password} = req.body;
 
+    if(!email || !password){
+        return res.status(400).json({
+            message : "Email and password are required!"
+        });
+    }
+
     try{
         const user = await User.findOne({email});
         if(!user){
@@ -114,4 +121,33 @@ export const logout = (_,res) => {
     res.status(200).json({
         message:"Logged out successfully!"
     });
+};
+
+export const updateProfile = async (req,res) => {
+    try{
+        const {profilePic} = req.body;
+        if(!profilePic){
+            return res.status(400).json({
+                message : "Profile pic is required",
+            });
+        }
+
+        const userId = req.user._id;
+
+        const uploadResponse = await cloudinary.uploader(profilePic);
+        const updatedUser = await User.findByIdAndUpdate(
+            userId,
+            {profilePic:uploadResponse.secure_url},
+            {new:true},
+        );
+
+        res.status(200).json({
+            updatedUser,
+        });
+    }catch(error){
+        console.error("error in update profile:",error);
+        res.status(500).json({
+            message : "Internal server error!",
+        });
+    }
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,13 @@ import {ENV} from "./lib/env.js";
 import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import {connectDB} from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const __dirname = path.resolve();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/auth',authRoutes);
 
