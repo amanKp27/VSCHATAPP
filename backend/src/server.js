6e198e8b0e48a428af7b89a4db8858c99d3f8912
Aman Kumar Poddar <amankrpoddar18@gmail.com>
2026-03-19 19:11:31 +0530

    cors integrated

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,11 +5,13 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import {connectDB} from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 const __dirname = path.resolve();
 
 app.use(express.json());
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
 app.use(cookieParser());
 
 app.use('/api/auth',authRoutes);
