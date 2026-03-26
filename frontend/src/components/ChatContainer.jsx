e3fa75369fafde264952561894914a9e4c8e2b49
Aman Kumar Poddar <amankrpoddar18@gmail.com>
2026-03-26 19:42:13 +0530

    send message implemented

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -5,20 +5,27 @@ import ChatHeader from "./ChatHeader";
 import NoChatHistoryPlaceholder from './NoChatHistoryPlaceholder';
 import MessageInput from "./MessageInput";
 import MessagesLoadingSkeleton from "./MessagesLoadingSkeleton";
-
+import { useRef } from 'react';
 function ChatContainer() {
   const {selectedUser,getMessagesByUserId,messages,isMessagesLoading} = useChatStore();
   const {authUser} = useAuthStore();
+  const messageEndRef = useRef(null);
 
   useEffect(()=>{
     getMessagesByUserId(selectedUser._id);
   },[selectedUser,getMessagesByUserId]);
 
+  useEffect(() => {
+    if(messageEndRef.current){
+      messageEndRef.current.scrollIntoView({behavior:"smooth"});
+    }
+  },[messages]);
+
   return (
     <>
       <ChatHeader />
       <div className='flex-1 px-6 overflow-y-auto py-8'>
-        {messages.length > 0 && isMessagesLoading ? (
+        {messages.length > 0 && !isMessagesLoading ? (
           <div className='max-w-3xl mx-auto space-y-6'> 
             {messages.map(msg=>(
               <div className={`chat ${msg.senderId===authUser._id ? "chat-end" : "chat-start"}`}>
@@ -32,11 +39,15 @@ function ChatContainer() {
                   )}
                   {msg.text && <p className='mt-2'>{msg.text}</p>}
                   <p className='text-xs mt-1 opacity-75 flex items-center gap-1'>
-                    {new Date(msg.createdAt).toISOString().slice(11,16)}
+                    {new Date(msg.createdAt).toLocaleTimeString(undefined,{
+                      hour:"2-digit",
+                      minute:"2-digit",
+                    })}
                   </p>
                 </div>
               </div>
             ))}
+            <div ref={messageEndRef} />
           </div>
         ) : isMessagesLoading ? <MessagesLoadingSkeleton /> : (
           <NoChatHistoryPlaceholder name={selectedUser.fullName} />
